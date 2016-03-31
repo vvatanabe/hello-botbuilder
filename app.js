17a9aff5789dd600d98859d3ccd4c750c959cb15
yuichi.watanabe <yuichi.watanabe.ja@gmail.com>
2016-03-31 22:09:21 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var builder = require('botbuilder');
+
+var helloBot = new builder.TextBot();
+helloBot.add('/', new builder.CommandDialog()
+    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
+    .matches('^quit', builder.DialogAction.endDialog())
+    .onDefault(function (session) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            session.send('Hello %s!', session.userData.name);
+        }
+    }));
+
+helloBot.add('/profile',  [
+    function (session) {
+        if (session.userData.name) {
+            builder.Prompts.text(session, 'What would you like to change it to?');
+        } else {
+            builder.Prompts.text(session, 'Hi! What is your name?');
+        }
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+helloBot.listenStdin();
