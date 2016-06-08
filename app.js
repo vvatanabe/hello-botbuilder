70151bb8e3f8ae030ecb3f5a12c65e4000027a53
yuichi.watanabe <yuichi.watanabe.ja@gmail.com>
2016-06-08 19:25:36 +0900

    update sample app and botbuilder

--- app.js
+++ app.js
@@ -1,29 +1,76 @@
-var builder = require('botbuilder');
+'use strict'
 
-var helloBot = new builder.TextBot();
-helloBot.add('/', new builder.CommandDialog()
-    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
-    .matches('^quit', builder.DialogAction.endDialog())
-    .onDefault(function (session) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
-        } else {
-            session.send('Hello %s!', session.userData.name);
-        }
-    }));
+const builder = require('botbuilder');
+const bot = new builder.TextBot();
 
-helloBot.add('/profile',  [
-    function (session) {
-        if (session.userData.name) {
-            builder.Prompts.text(session, 'What would you like to change it to?');
-        } else {
-            builder.Prompts.text(session, 'Hi! What is your name?');
-        }
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
+bot.use((session, next) => {
+  // middleware1
+  next();
+})
+bot.use((session, next) => {
+  // middleware2
+  next();
+})
+
+const dialog = new builder.CommandDialog();
+
+dialog.onDefault(() => {
+  // dafault
+})
+
+dialog.matches('register profile', [
+  (session) => {
+    session.beginDialog('/profile/name');
+  },
+  (session) => {
+    session.beginDialog('/profile/age');
+  },
+  (session) => {
+    session.beginDialog('/profile/confirm');
+  },
+  (session) => {
+    session.reset('/')
+  }
+])
+
+bot.add('/profile/name', [
+  (session) => {
+    builder.Prompts.text(session, 'What\'s your name ?');
+  },
+  (session, results) => {
+    session.userData.name = results.response;
+    session.endDialog();
+  }
 ]);
 
-helloBot.listenStdin();
+bot.add('/profile/age', [
+  (session) => {
+    builder.Prompts.number(session, 'How old are you ?');
+  },
+  (session, results) => {
+    session.userData.age = results.response;
+    session.endDialog();
+  }
+])
+
+bot.add('/profile/confirm', [
+  (session) => {
+    const message = `
+      Your name: ${session.userData.name}
+      Your age: ${session.userData.age}
+      Are you sure ?
+    `;
+    builder.Prompts.confirm(session, message);
+  },
+  (session, results) => {
+    if (results.response) {
+      session.send('Completed the profile registration.');
+    } else {
+      session.send('Canceled profile registration.');
+    }
+    session.endDialog();
+  }
+])
+
+bot.add('/', dialog);
+bot.listenStdin();
